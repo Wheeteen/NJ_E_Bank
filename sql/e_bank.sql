ea75dc7286a7e0f7a042f05c89d984196455ba74
diang <1970234657@qq.com>
2017-05-19 09:45:28 +0800

    sql

--- sql/e_bank.sql
+++ sql/e_bank.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 2017-05-11 08:22:37
+-- Generation Time: 2017-05-19 03:44:20
 -- 服务器版本： 10.1.19-MariaDB
 -- PHP Version: 5.6.28
 
@@ -51,25 +51,23 @@ CREATE TABLE `customer_info` (
   `Cid` varchar(20) NOT NULL,
   `Cname` varchar(30) NOT NULL,
   `PIN` varchar(40) NOT NULL,
-  `Cnumber` varchar(16) NOT NULL,
-  `Anumber` varchar(14) NOT NULL,
-  `status` varchar(18) NOT NULL,
-  `balance` int(20) NOT NULL
+  `Cnumber` varchar(20) NOT NULL,
+  `Anumber` varchar(18) NOT NULL,
+  `status` int(10) NOT NULL,
+  `balance` int(20) NOT NULL,
+  `Uaccount` varchar(45) DEFAULT NULL,
+  `Email` varchar(40) DEFAULT NULL,
+  `Upwd` varchar(45) DEFAULT NULL,
+  `codeRster` varchar(40) DEFAULT NULL,
+  `codePwd` varchar(40) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- 转存表中的数据 `customer_info`
 --
 
-INSERT INTO `customer_info` (`Cid`, `Cname`, `PIN`, `Cnumber`, `Anumber`, `status`, `balance`) VALUES
-('123', 'Jessy', '5DEDAF99D256C1CFD97CE2975F4B8624', '7794031046962995', '60792033286835', '0', 51100),
-('5689', 'Mery', '502E4A16930E414107EE22B6198C578F', '7690073845204719', '42028231558359', '0', 0),
-('725713', 'Nick', 'F401B51D4F52C1D427911F7041392AA2', '6165447250033919', '34255285735054', '0', 0),
-('78755', 'Jesscia', '024D7F84FFF11DD7E8D9C510137A2381', '7439246675335062', '47242827361233', '0', 0),
-('7889', 'Susan', '8B6DD7DB9AF49E67306FEB59A8BDC52C', '7571675253994344', '24350588439959', '0', 0),
-('789', 'Dorry', '68053AF2923E00204C3CA7C6A3150CF7', '2874324596367932', '15226696456237', '0', 0),
-('87654', 'Tony', '024D7F84FFF11DD7E8D9C510137A2381', '9225614298670717', '47195296213400', '0', 0),
-('8976', 'Tracy', '621461AF90CADFDAF0E8D4CC25129F91', '4064084334830211', '54200992820472', '0', 0);
+INSERT INTO `customer_info` (`Cid`, `Cname`, `PIN`, `Cnumber`, `Anumber`, `status`, `balance`, `Uaccount`, `Email`, `Upwd`, `codeRster`, `codePwd`) VALUES
+('440981199509111724', 'Me', 'E10ADC3949BA59ABBE56E057F20F883E', '5985604409731612', '24440772460151', 2, 0, 'Jessy', '1970234657@qq.com', 'B005619EC5C843D850A33CC94ACF7B01', '8b311f2b5d8f4e30b2f1a6e684ab0978', NULL);
 
 -- --------------------------------------------------------
 
@@ -96,7 +94,23 @@ INSERT INTO `record_info` (`id`, `Anumber`, `amount`, `balance`, `currency`, `Ot
 (13, '60792033286835', 200, 100, 'CNY', 'Withdraw', '2017-05-11 05:39:57'),
 (14, '60792033286835', 50000, 50100, 'CNY', 'Deposit', '2017-05-11 05:41:19'),
 (15, '60792033286835', 50000, 100, 'CNY', 'Withdraw', '2017-05-11 05:41:50'),
-(16, '60792033286835', 51000, 51100, 'CNY', 'Deposit', '2017-05-11 05:48:26');
+(16, '60792033286835', 51000, 51100, 'CNY', 'Deposit', '2017-05-11 05:48:26'),
+(17, '26842124189509', 600, 600, 'CNY', 'Deposit', '2017-05-18 07:12:03'),
+(18, '26842124189509', 600, 1200, 'CNY', 'Deposit', '2017-05-18 07:12:50'),
+(19, '26842124189509', 500, 1700, 'CNY', 'Deposit', '2017-05-18 07:13:38'),
+(20, '26842124189509', 80000, 81700, 'CNY', 'Deposit', '2017-05-18 07:14:05'),
+(21, '26842124189509', 90000, 171700, 'CNY', 'Deposit', '2017-05-18 07:14:57'),
+(22, '26842124189509', 90000, 261700, 'CNY', 'Deposit', '2017-05-18 07:15:12'),
+(23, '26842124189509', 50312, 211388, 'CNY', 'Withdraw', '2017-05-18 07:15:56'),
+(24, '26842124189509', 600, 211988, 'CNY', 'Deposit', '2017-05-18 07:20:38'),
+(25, '26842124189509', 300, 211688, 'CNY', 'Withdraw', '2017-05-18 07:23:14'),
+(26, '26842124189509', 100000, 111688, 'CNY', 'Withdraw', '2017-05-18 07:24:07'),
+(27, '26842124189509', 200, 111488, 'CNY', 'Withdraw', '2017-05-18 07:25:21'),
+(28, '26842124189509', 300, 111188, 'CNY', 'Withdraw', '2017-05-18 07:25:43'),
+(29, '26842124189509', 50000, 61188, 'CNY', 'Withdraw', '2017-05-18 07:25:57'),
+(30, '26842124189509', 300, 60888, 'CNY', 'Withdraw', '2017-05-18 07:27:45'),
+(31, '26842124189509', 50000, 10888, 'CNY', 'Withdraw', '2017-05-18 07:27:58'),
+(32, '26842124189509', 80000, 90888, 'CNY', 'Deposit', '2017-05-18 07:28:15');
 
 --
 -- Indexes for dumped tables
@@ -133,7 +147,7 @@ ALTER TABLE `administrator_info`
 -- 使用表AUTO_INCREMENT `record_info`
 --
 ALTER TABLE `record_info`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
